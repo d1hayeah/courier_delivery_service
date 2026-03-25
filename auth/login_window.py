c3c2ba404d323d0bf2a3a67caeae78f2ec4a10a8
d1hayeah <166990130+d1hayeah@users.noreply.github.com>
2026-03-25 17:45:43 +0300

    Исправление ошибки

--- auth/login_window.py
+++ auth/login_window.py
@@ -30,18 +30,20 @@ class LoginWindow(QWidget):
         self.setLayout(layout)
 
     def open_register(self):
-        from register_window import RegisterWindow
+        from .register_window import RegisterWindow
         self.reg_win = RegisterWindow()
         self.reg_win.show()
 
     def check_login(self):
         username = self.input_login.text().strip()
         password = self.input_password.text().strip()
+        
         if not username or not password:
             QMessageBox.warning(self, "Ошибка", "Введите логин и пароль!")
             return
         
         hashed = hashlib.sha256(password.encode()).hexdigest()
+        
         conn = get_connection()
         if not conn:
             QMessageBox.critical(self, "Ошибка", "Нет связи с БД")
@@ -50,14 +52,18 @@ class LoginWindow(QWidget):
         cursor = conn.cursor()
         cursor.execute("SELECT id, username, role FROM users WHERE username = %s AND password = %s;", (username, hashed))
         user = cursor.fetchone()
+        if not user:
+            cursor.execute("SELECT id, username, role FROM users WHERE username = %s AND password = %s;", (username, password))
+            user = cursor.fetchone()
+        
         cursor.close()
         conn.close()
         
         if user:
-            user_id, username, role = user
+            user_id, db_username, role = user
             from modules.menu_window import MenuWindow
             self.hide()
-            self.menu = MenuWindow(username, role, user_id)
+            self.menu = MenuWindow(db_username, role, user_id)
             self.menu.show()
         else:
             QMessageBox.warning(self, "Ошибка", "Неверный логин или пароль")
